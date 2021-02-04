4afeb220ba510765cd2a94e35634d0d9a7f539da
subbotin <subbotin@haulmont.com>
2021-02-04 18:07:13 +0400

    Hibernate merge into master
    Rename eclipselink modules

--- dynattr/src/test/groovy/dynamic_attributes/DynamicAttributesTest.groovy
+++ dynattr/src/test/groovy/dynamic_attributes/DynamicAttributesTest.groovy
@@ -23,7 +23,7 @@ import io.jmix.core.Metadata
 import io.jmix.core.entity.EntityValues
 import io.jmix.data.DataConfiguration
 import io.jmix.data.entity.ReferenceToEntity
-import io.jmix.dataeclipselink.DataEclipselinkConfiguration
+import io.jmix.eclipselink.EclipselinkConfiguration
 import io.jmix.dynattr.AttributeType
 import io.jmix.dynattr.DynAttrConfiguration
 import io.jmix.dynattr.DynAttrMetadata
@@ -43,7 +43,7 @@ import test_support.entity.UserRole
 import javax.sql.DataSource
 
 @ContextConfiguration(
-        classes = [CoreConfiguration, DataConfiguration, DataEclipselinkConfiguration,
+        classes = [CoreConfiguration, DataConfiguration, EclipselinkConfiguration,
                 DynAttrConfiguration, JmixDynAttrTestConfiguration],
         initializers = [TestContextInititalizer]
 )
